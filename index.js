50dfbf8b414901b4ef05a146d457ae7c7954f216
Koreex Jin <koreex.jin@gmail.com>
2023-06-27 04:47:14 -0500

    add code for auto update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron");
+const path = require("path");
+const { autoUpdater } = require("electron-updater");
+
 let mainWindow;
-function createWindow () {
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
-    overflow:'hidden',
+    overflow: "hidden",
     frameless: true,
     kiosk: false,
     resizable: true,
@@ -14,60 +17,68 @@ function createWindow () {
     autoHideMenuBar: true,
 
     webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        enableRemoteModule: true,
-        preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      preload: path.join(__dirname, "preload.js"),
     },
-  })
+  });
 
   // Lock window at 16:9
   mainWindow.setAspectRatio(16 / 9);
   // End Lock window at 16:9
 
-  let page = 'home';
-  mainWindow.loadFile('src/index.html')
+  let page = "home";
+  mainWindow.loadFile("src/index.html");
 
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Register the shortcut for fullscreen mode
-  globalShortcut.register('F11', () => {
-    mainWindow.setFullScreen(!mainWindow.isFullScreen())
-  })
+  globalShortcut.register("F11", () => {
+    mainWindow.setFullScreen(!mainWindow.isFullScreen());
+  });
 
   // Register the shortcut for exiting fullscreen mode
-  globalShortcut.register('Escape', () => {
-    mainWindow.setFullScreen(false)
-  })
+  globalShortcut.register("Escape", () => {
+    mainWindow.setFullScreen(false);
+  });
 }
 
 // Minimize and close window
-ipcMain.on('minimize-window', () => {
+ipcMain.on("minimize-window", () => {
   mainWindow.minimize();
 });
 
-ipcMain.on('close-window', () => {
+ipcMain.on("close-window", () => {
   mainWindow.close();
 });
 
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+});
+
+ipcMain.on("check_update", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 
-  app.on('will-quit', () => {
+  app.on("will-quit", () => {
     // Unregister all shortcuts
-    globalShortcut.unregisterAll()
-  })
-})
+    globalShortcut.unregisterAll();
+  });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // Path: preload.js
 try {
-  require('electron-reloader')(module)
+  require("electron-reloader")(module);
 } catch (_) {}
